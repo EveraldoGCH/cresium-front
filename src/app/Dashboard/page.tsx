4b0a0f3f79a66994e2eccce2a0984af276fe6fd7
EveraldoGCH <everaldoguevara@gmail.com>
2024-04-16 00:11:28 -0300

    config de husky

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -3,6 +3,8 @@ import React, {useState} from 'react'
 export default function DashBoard() {
   const [lol]=useState(true)
   console.log("XD")
+  console.log("XD")
+
   return (
     <div>page dash</div>
   )
