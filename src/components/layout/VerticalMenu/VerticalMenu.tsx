c8f23954de8b6a6b014b455016824987fed624c6
EveraldoGCH <everaldoguevara@gmail.com>
2024-04-16 19:38:17 -0300

    agregando iconos y nuevo sidenav

--- src/components/layout/VerticalMenu/VerticalMenu.tsx
+++ src/components/layout/VerticalMenu/VerticalMenu.tsx
@@ -1,74 +1,25 @@
 "use client";
 import * as React from "react";
-import Box from "@mui/material/Box";
-import GlobalStyles from "@mui/material/GlobalStyles";
-// import { useSettings } from '@/hooks/use-settings';
-// import { layoutConfig } from '../config';
-// import { SideNav } from './side-nav';
 import { VerticalMenuProps } from "./VerticalMenuProps";
-import { SideNav } from "../SideNav/SideNav";
-import { layoutConfig } from "../SideNav/layoutItems";
+import { FirstBlock, ListItemNav, LogoCont, SideNavCont } from "./styles";
+import Image from "next/image";
+import logo from "../../../../public/assets/logo.svg"
 
 export function VerticalMenu({
   children,
-}: VerticalMenuProps): React.JSX.Element {
-  // const { settings } = useSettings();
+}: VerticalMenuProps) {
 
   return (
-    <React.Fragment>
-      <GlobalStyles
-        styles={{
-          body: {
-            "--MainNav-height": "56px",
-            "--MainNav-zIndex": 1000,
-            "--SideNav-width": "280px",
-            "--SideNav-zIndex": 1100,
-            "--MobileNav-width": "320px",
-            "--MobileNav-zIndex": 1100,
-          },
-        }}
-      />
-      <Box
-        sx={{
-          bgcolor: "var(--mui-palette-background-default)",
-          display: "flex",
-          flexDirection: "column",
-          position: "relative",
-          minHeight: "100%",
-        }}
-      >
-        <SideNav
-          color={"evident"}
-          items={layoutConfig.navItems}
-          secondaryItems={layoutConfig.secondaryNavItems}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            flex: "1 1 auto",
-            flexDirection: "column",
-            pl: { lg: "var(--SideNav-width)" },
-          }}
-        >
-          <Box
-            component="main"
-            sx={{
-              "--Content-margin": "0 auto",
-              "--Content-maxWidth": "var(--maxWidth-xl)",
-              "--Content-paddingX": "24px",
-              "--Content-paddingY": { xs: "24px", lg: "64px" },
-              "--Content-padding":
-                "var(--Content-paddingY) var(--Content-paddingX)",
-              "--Content-width": "100%",
-              display: "flex",
-              flex: "1 1 auto",
-              flexDirection: "column",
-            }}
-          >
-            {children}
-          </Box>
-        </Box>
-      </Box>
-    </React.Fragment>
+    <SideNavCont>
+      <LogoCont>
+        <Image src={logo} alt="Logo" />
+      </LogoCont>
+      <FirstBlock>
+        <ListItemNav>
+          
+        </ListItemNav>
+      </FirstBlock>
+
+    </SideNavCont>
   );
 }
