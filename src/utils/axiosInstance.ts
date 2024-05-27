6fc4f16a8f34d021032b048159796dc55a0f8d1e
EveraldoGCH <everaldoguevara@gmail.com>
2024-05-27 16:13:32 -0300

    fix en url para mock

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosInstance } from "axios";
 
+const isMock = process.env.NEXT_PUBLIC_MOCK === "true";
+
 const axiosInstance: AxiosInstance = axios.create({
   // eslint-disable-next-line
-  baseURL: process.env.NEXT_PUBLIC_MOCK
+  baseURL: isMock
     ? process.env.NEXT_PUBLIC_BASE_URL_MOCK
-    : "",
+    : window.location.origin,
 });
 
 export default axiosInstance;
